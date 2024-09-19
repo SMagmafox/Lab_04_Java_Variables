86eeb66cd670eb43c70de404d3ab2630aa615231
Christian <cristlethedjfurry@gmail.com>
2024-09-19 13:41:52 -0400

    Completed Part 1

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int int0perandA = 49;
+        int int0perandB = 55;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        double double0perandA = 44.2;
+        double double0perandB = 22.6;
+
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+        double doubleModulo = 0;
+
+
+        intSum = int0perandA + int0perandB;
+        System.out.println("the sum using two ints of " + int0perandA + " and " + int0perandB + " is " + intSum);
+
+        intProduct = int0perandA * int0perandB;
+        System.out.println("the product using two ints of " + int0perandA + " and " + int0perandB + " is " + intProduct);
+
+        intDifference = int0perandA - int0perandB;
+        System.out.println("the difference using two ints of " + int0perandA + " and " + int0perandB + " is " + intDifference);
+
+        intQuotient = int0perandA / int0perandB;
+        System.out.println("the quotient using two ints of " +int0perandA + " and " + int0perandB + " is " + intQuotient);
+
+        intModulo = int0perandA % int0perandB;
+        System.out.println("the modulo using two ints of " + int0perandA + " and " + int0perandB + " is " + intModulo);
+
+
+
+        doubleSum = double0perandA + double0perandB;
+        System.out.println("the sum using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleSum);
+
+        doubleProduct = double0perandA * double0perandB;
+        System.out.println("the product using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleProduct);
+
+        doubleDifference = double0perandA - double0perandB;
+        System.out.println("the difference using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleDifference);
+
+        doubleQuotient = double0perandA / double0perandB;
+        System.out.println("the quotient using two doubles of " +double0perandA + " and " + double0perandB + " is " + doubleQuotient);
+
+        doubleModulo = double0perandA % double0perandB;
+        System.out.println("the modulo using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleModulo);
+
+        int kidsInFamily = 3;
+        boolean isRainingYN = false;
+        double gasPrice = 03.40;
+        int favNum = 4;
+        double shoeSize = 13.5;
+        int birthMonth = 9;
+        String fullName = "Christian Smith";
+
+    }
+}
